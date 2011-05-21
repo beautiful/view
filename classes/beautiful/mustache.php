2691d0538cd6c9e3d66067723a062285a6ef5969
Luke Morton <lukemorton.designs@gmail.com>
2011-05-21 15:24:28 +0100

    Change EOL.

--- classes/beautiful/mustache.php
+++ classes/beautiful/mustache.php
@@ -1,1 +1,3 @@
-<?php defined('SYSPATH') or die('No direct script access.');require_once dirname(__FILE__).'../../../vendor/mustache.php/Mustache.php';class Beautiful_Mustache extends Mustache {}
+<?php defined('SYSPATH') or die('No direct script access.');
+require_once dirname(__FILE__).'../../../vendor/mustache.php/Mustache.php';
+class Beautiful_Mustache extends Mustache {}
